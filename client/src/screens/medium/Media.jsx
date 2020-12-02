8fe58e20d1550384b2b1101e796c5967c153aec8
Eddie <edward.f.didonato@gmail.com>
2020-12-02 16:27:48 -0500

    style media page

--- client/src/screens/medium/Media.jsx
+++ client/src/screens/medium/Media.jsx
@@ -5,21 +5,26 @@ import MediaStyles from '../../stylesheets/MediaStyles';
 export default function Media({ media }) {
   // const { media } = props
 
+
+
   return (
     <MediaStyles>
       <h1>Media</h1>
       <div className="media-container">
-
         {
           media.map((medium) => (
-            <React.Fragment key={medium.id}>
-              <Link to={`/media/${medium.id}`}>
-                <h2>{medium.name}</h2>
-                <img src={medium.img_url} />
-              </Link>
-              <>
-                <p>{medium.description}</p>
-              </>
+            <React.Fragment key={medium.id} >
+              <div className="media-card">
+                <div id="image-container">
+                  <img src={medium.img_url} />
+                </div>
+                <div className="media-card-right">
+                  <Link to={`/media/${medium.id}`}>
+                    <h2>{medium.name}</h2>
+                  </Link>
+                  <p>{medium.description}</p>
+                </div>
+              </div>
             </React.Fragment>
           ))
         }
--- client/src/stylesheets/MediaStyles.js
+++ client/src/stylesheets/MediaStyles.js
@@ -1,8 +1,81 @@
 import styled from 'styled-components';
 
 const MediaStyles = styled.div`
-img {
+display: flex;
+flex-direction: column;
+align-items: center;
 
+.media-container {
+width: 75vw;
+min-width: 750px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 20px;
+background: #F9EAE7;
+}
+
+a {
+  text-decoration: none;
+}
+
+h2 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #09274140;
+  margin: 0;
+  height: 60px;
+  width: 100%;
+  text-decoration: none;
+  color: #E36562;
+}
+
+p {
+  display: flex;
+  padding: 20px;
+  background: white;
+  height: 200px;
+  margin: 0;
+}
+
+.media-card {
+  margin: 20px 0;
+display: flex;
+width: 100%;
+min-width: 500px;
+max-width: 800px;
+height: 300px;
+
+}
+
+#image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+width: 300px;
+min-width: 300px;
+height: 300px;
+/* background: #092741; */
+background: rgba(9, 39, 65, .9);
+}
+
+.media-card-right {
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+width: 100%;
+  min-width: 420px;
+
+}
+
+img {
+border-radius: 50%;
+/* border: 2px solid rgb(249, 234, 231); */
+object-fit: cover;
+width: 220px;
+height: 220px;
 }
 
 `

82994253f9f79273bb8b2891edf878a6c555a6d0
Eddie <edward.f.didonato@gmail.com>
2020-12-04 15:34:35 -0500

    deploy

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
 
-// const baseUrl = heroku-link
-// const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://school-app-test.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://colab-eddie.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl

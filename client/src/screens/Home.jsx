610881096352840cd519a2afc1345e68eb525022
Eddie <edward.f.didonato@gmail.com>
2020-12-05 16:29:41 -0500

    rework home page

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -9,13 +9,46 @@ export default function Home() {
   return (
     <HomeStyled>
       <div className="home-main">
-        <h1>C o L <FontAwesomeIcon icon={faFlask} id="home-flask"/> b</h1>
-        <h2>connecting creative-doers of every medium</h2>
+        <div className="home-title-container">
+          <div id="home-title">
+            <div id="title-top">
+              <h1 >C o L  <FontAwesomeIcon icon={faFlask} id="home-flask" />  b</h1>
+              <h2>connecting creative-doers of every medium</h2>
+            </div>
+            <small>[anything done with grace and passion is a work of art]</small>
+
+          </div>
+          {/* <div className="home-button-container">
+            <Link to="/login" className="home-link"><button>log in</button></Link>
+            <Link to="/register" className="home-link"><button>create an account</button></Link>
+          </div> */}
+        </div>
+
+        <div className="home-about">
+          <h2>About CoLab</h2>
+          <h4>CoLab was born from the belief that the best art comes from collaboration.  </h4>
+          <p>While there are many platforms out there for artists to build communities within their medium, we
+            wanted to create a place where artists of any medium can collaborate beyond their niche. </p>
+          <p>Giving new meaning to the idea of mixed-media, CoLab is here to provide the opportunity for
+          unimaginable fusions of art forms. Each medium has its own messageboard where artists can propose new collaborations,
+            offer their services, or search for someone within that medium to join in on a project.</p>
+          <p>It's known that the most creative people have introverted tendencies, which makes it difficult for
+            many of us to put ourselves out there. </p>
+          <p>At CoLab, we want to form a culture of inclusion and appreciation, so the most creative 
+          people in the world can find each other.
+          </p>
+          <p>Interested? <Link className="start" to="/register">Let's get started.</Link></p>
+
+        </div>
+
+        
+
+        {/* <h2>connecting creative-doers of every medium</h2>
         <p>[anything done with grace and passion is a work of art]</p>
         <div className="home-button-container">
-        <Link to="/login" className="home-link"><button>log in</button></Link>
-        <Link to="/register" className="home-link"><button>create an account</button></Link>
-        </div>
+          <Link to="/login" className="home-link"><button>log in</button></Link>
+          <Link to="/register" className="home-link"><button>create an account</button></Link>
+        </div> */}
       </div>
     </HomeStyled>
 
--- client/src/stylesheets/Home.js
+++ client/src/stylesheets/Home.js
@@ -1,62 +1,138 @@
 import styled from 'styled-components';
 
 const HomeStyled = styled.div`
-background: url("https://images.unsplash.com/photo-1504441221322-7525a924d341?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dW1icmVsbGFzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60");
-background-size: cover;
 height: 100%;
 width: 100%;
-display: flex;
+/* display: flex; */
 flex-direction: column;
 align-items: center;
 justify-content: center;
 overflow: hidden;
-margin-bottom: -60px;
+overflow-y: scroll;
+margin-bottom: 60px;
+background: #F1F0F0;
 
 
-.home-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  line-height: 40px;
-  padding: 20px;
-/* background: #092741; */
-/* color: #F1F0F0; */
-  color: #092741;
-  background: rgba(249, 234, 231, .7);
-  border: 4px solid rgba(249, 234, 231, .3);
+.home-title-container {
+  background-image: url("https://cdn.shopify.com/s/files/1/1863/2463/products/MIAMI_2_345x345@2x.jpg?v=1571512336");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  height:90vh;
+  width: 100vw;
+  /* margin-top: 170px; */
 }
 
+#home-title {
+  background-color: rgb(245, 245, 245, .5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  margin: 0;
+  height: 100%;
+  width: 100vw;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  mix-blend-mode: screen;
+}
+
+#title-top {
+  background-color: rgb(245, 245, 245, 1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20vw;
+  padding: 20px;
+  width: 100vw;
+}
+
+
 h1 {
-  font-family: avenir;
-  font-weight: 600;
-  font-size: 60px;
+  font-size: 10vw; 
+  margin: 0;
+  
 }
 
 #home-flask {
   color: #E36562;
+  padding: 0 1vw;
 }
 
 h2 {
   margin-top: 0;
   font-weight: 300;
+  text-align: center;
+}
+h4 {
+  text-align: center;
 }
 
-p {
+small {
   font-style: italic;
   font-weight: 200;
-  color: #F1F0F0;
+  font-size: 18px;
+  color: black;
 }
 
 .home-button-container {
+  position: absolute;
+  bottom: 160px;
   display: flex;
   justify-content: space-between;
   width: 200px;
+  z-index: 1;
 }
 
 .home-link {
   text-decoration: none;
   color: #F1F0F0;
+}
+
+.home-about {
+  display: flex;
+  flex-direction: column;
+  width: 580px;
+  margin: 0 auto;
+  margin-top: 100px;
+  text-align: justify;
+  color: #092741;
+  margin-bottom: 100px;
+  border-right: 1px solid #E36562;
+  padding: 0 40px;
+  border-left: 1px solid #E36562;
+}
+
+.start {
+  color: #E36562;
+  font-weight: 600;
+}
+
+@media screen and (max-width: 600px) {
+  #title-top {
+    height: 40vw;
+  }
+
+  h1 {
+  font-size: 20vw;
+}
+
+h2 {
+  font-size: 28px;
+}
+
+small {
+  font-size: 15px;
+}
+
+.home-about {
+  width: 80vw;
+}
+
 }
 
 

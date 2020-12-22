7efb17e4e9e0176d01f007b16ece9a001fe838b0
Eddie <edward.f.didonato@gmail.com>
2020-12-22 15:46:23 -0500

    can remove filters from search

--- client/src/screens/user/Users.jsx
+++ client/src/screens/user/Users.jsx
@@ -9,6 +9,7 @@ export default function Users(props) {
   const [mediaFilter, setMediaFilter] = useState([])
   const [searchUser, setSearchUser] = useState('')
   const [searchByUser, setSearchByUser] = useState(false)
+  const [updatedFilter, setUpdatedFilter] = useState(false);
 
   useEffect(() => {
     searchUser.length ? setSearchByUser(true) : setSearchByUser(false)
@@ -16,16 +17,18 @@ export default function Users(props) {
       user.media.some(medium =>
         mediaFilter.includes(medium.name)
       )
-      ||
-        searchByUser ? 
+        ||
+        searchByUser ?
         user.username.toLowerCase().includes(searchUser.toLowerCase())
         :
         null
     ))
-  }, [users, mediaFilter, searchUser, searchByUser])
-  
-  const showAllUsers = (e) => {
-    setShowUsers(users)
+  }, [users, mediaFilter, searchUser, searchByUser, updatedFilter])
+
+  const removeFilter = (idx) => {
+    mediaFilter.splice(idx, 1)
+    setMediaFilter(mediaFilter)
+    setUpdatedFilter(!updatedFilter);
   }
 
   // to collaborate, logged in user clicks a button on user's profile that takes you to a 
@@ -36,7 +39,8 @@ export default function Users(props) {
 
   return (
     <UserList>
-      <h1>users</h1>
+      <h1>find users</h1>
+
       <form onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
@@ -44,24 +48,34 @@ export default function Users(props) {
           onChange={(e) => setSearchUser(e.target.value)}
         />
         <select onChange={(e) => setMediaFilter([...mediaFilter, e.target.value])}>
-        <option disabled value='default' >find by media</option>
-          {media.map(medium => 
-            <option value={medium.name}>{ medium.name}</option>
-            )}
+          <option disabled value='default' >find by media</option>
+          {media.map(medium =>
+            <option value={medium.name}>{medium.name}</option>
+          )}
         </select>
       </form>
+      <ul className="media-filters">
+        {mediaFilter.map((medium, idx) => 
+          <div className="filter-list">
+            <li>{medium}</li>
+            <small onClick={() => removeFilter(idx)}>x</small>
+          </div>
+        )}
+      </ul>
       {showUsers?.map(user =>
         <div className="user-thumbnail">
-          <Link  className="user-link" key={user.id} to={`/users/${user.id}`}>
-            <img  className="user-thumbnail-img" src={ user.img_url ? user.img_url : `https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60`}/>
-            <p className="thumbnail-username">{user.username}</p>
-            <p>matching media:</p>
-            {user.media.filter(medium => 
-              mediaFilter.includes(medium.name)
-            ).map(medium => 
-              <p>{ medium.name}</p>
-              )}
+          <Link className="user-link" key={user.id} to={`/users/${user.id}`}>
+            <img className="user-thumbnail-img" src={user.img_url ? user.img_url : `https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60`} />
+            <h3 className="thumbnail-username">{user.username}</h3>
           </Link>
+          <h4>matching media:</h4>
+          <ul>
+            {user.media.filter(medium =>
+              mediaFilter.includes(medium.name)
+            ).map(medium =>
+              <li>{medium.name}</li>
+            )}
+          </ul>
         </div>
       )}
     </UserList>

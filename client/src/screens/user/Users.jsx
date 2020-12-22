c525d2071efb3a508396240c14882fccbaaea20c
Eddie <edward.f.didonato@gmail.com>
2020-12-22 16:31:57 -0500

    add light styling

--- client/src/screens/user/Users.jsx
+++ client/src/screens/user/Users.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import UserList from '../../stylesheets/UserList';
+import FormStyles from '../../stylesheets/FormStyles';
 
 export default function Users(props) {
   const { users, media } = props;
@@ -38,46 +39,64 @@ export default function Users(props) {
   // to add to the collaboration
 
   return (
-    <UserList>
-      <h1>find users</h1>
-
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          value={searchUser}
-          onChange={(e) => setSearchUser(e.target.value)}
-        />
-        <select onChange={(e) => setMediaFilter([...mediaFilter, e.target.value])}>
-          <option disabled value='default' >find by media</option>
-          {media.map(medium =>
-            <option value={medium.name}>{medium.name}</option>
+    <UserList mediaFilter={mediaFilter}>
+      <div className="search-area">
+        <h1>find users</h1>
+        <FormStyles>
+          <form className="form-main" id="user-filter" onSubmit={(e) => e.preventDefault()}>
+            <label id="username">search username
+          <input
+                name="user-search"
+                id="user-search"
+                type="text"
+                value={searchUser}
+                onChange={(e) => setSearchUser(e.target.value)}
+              />
+            </label>
+            <label> filter by media
+              <select id="user-media-filter" onChange={(e) => setMediaFilter([...mediaFilter, e.target.value])}>
+                <option disabled value='default' >find by media</option>
+                {media.map(medium =>
+                  <option value={medium.name}>{medium.name}</option>
+                )}
+              </select>
+            </label>
+          </form>
+        </FormStyles>
+        <ul className="media-filters">
+          {mediaFilter.map((medium, idx) =>
+            <div className="filter-list">
+              <li className="filter-medium-name">{medium}</li>
+              <small className="remove-filter" onClick={() => removeFilter(idx)}>x</small>
+            </div>
           )}
-        </select>
-      </form>
-      <ul className="media-filters">
-        {mediaFilter.map((medium, idx) => 
-          <div className="filter-list">
-            <li>{medium}</li>
-            <small onClick={() => removeFilter(idx)}>x</small>
+        </ul>
+      </div>
+      <div className="show-users-container">
+        {!showUsers.length ? 
+          <p>no users match criteria</p>
+          : 
+          <></>
+          }
+        {showUsers?.map(user =>
+          <div className="user-thumbnail">
+            <Link className="user-link" key={user.id} to={`/users/${user.id}`}>
+              <img className="user-thumbnail-img" src={user.img_url ? user.img_url : `https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60`} />
+              <h3 className="thumbnail-username">{user.username}</h3>
+            </Link>
+            <div className="matches">
+              <h4 className="matches-title">matches:</h4>
+              <ul>
+                {user.media.filter(medium =>
+                  mediaFilter.includes(medium.name)
+                ).map(medium =>
+                  <li className="matches-li">{medium.name}</li>
+                )}
+              </ul>
+            </div>
           </div>
         )}
-      </ul>
-      {showUsers?.map(user =>
-        <div className="user-thumbnail">
-          <Link className="user-link" key={user.id} to={`/users/${user.id}`}>
-            <img className="user-thumbnail-img" src={user.img_url ? user.img_url : `https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60`} />
-            <h3 className="thumbnail-username">{user.username}</h3>
-          </Link>
-          <h4>matching media:</h4>
-          <ul>
-            {user.media.filter(medium =>
-              mediaFilter.includes(medium.name)
-            ).map(medium =>
-              <li>{medium.name}</li>
-            )}
-          </ul>
-        </div>
-      )}
+      </div>
     </UserList>
   )
 }
--- client/src/stylesheets/UserList.js
+++ client/src/stylesheets/UserList.js
@@ -1,14 +1,33 @@
 import styled from 'styled-components';
 
 const UserList = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
 
+.search-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: white;
+  padding-bottom: ${({mediaFilter}) => (mediaFilter.length ? "0px" : "55px")}
+}
+
+.show-users-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 65vw;
+  margin: 0 auto;
+  margin-top: 50px;
+  background: white;
+}
 
 .user-thumbnail {
   display: flex;
   flex-direction: column;
   width: 300px;
-  background: yellow;
-  margin: 30px;
+  margin: 10px;
 }
 
 .user-link {
@@ -26,6 +45,61 @@ const UserList = styled.div`
 
 .thumbnail-username {
   font-size: 18px;
+}
+
+.media-filters {
+  display: flex;
+}
+
+select {
+  width: 100%;
+  background: #61485380;
+  height: 30px;
+  border: none;
+}
+
+select:hover {
+  background: rgba(97, 72, 83, .7);
+}
+
+select:focus {
+  outline: none;
+}
+
+.filter-list {
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border: 1px solid gray;
+  margin: 10px;
+}
+
+.remove-filter {
+  margin-left: 5px;
+  cursor: default;
+}
+
+#username {
+  /* width: 100%; */
+}
+
+#user-filter {
+  width: 500px;
+  height: 100px;
+  padding: 20px;
+}
+
+#user-media-filter {
+  width: 70%;
+}
+
+ul {
+  padding-left: 0;
+}
+
+.matches-title {
+  margin-bottom: 0px;
+
 }
 
 `

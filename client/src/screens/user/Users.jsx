683bfcfc562a31cd51e7d2b1819e826dfd86021b
Eddie <edward.f.didonato@gmail.com>
2020-12-22 15:02:26 -0500

    search function works

--- client/src/screens/user/Users.jsx
+++ client/src/screens/user/Users.jsx
@@ -7,15 +7,26 @@ export default function Users(props) {
 
   const [showUsers, setShowUsers] = useState(users)
   const [mediaFilter, setMediaFilter] = useState([])
+  const [searchUser, setSearchUser] = useState('')
+  const [searchByUser, setSearchByUser] = useState(false)
 
   useEffect(() => {
+    searchUser.length ? setSearchByUser(true) : setSearchByUser(false)
     setShowUsers(users.filter(user =>
       user.media.some(medium =>
         mediaFilter.includes(medium.name)
-        )
-      ))
-  },[users,  mediaFilter])
-
+      )
+      ||
+        searchByUser ? 
+        user.username.toLowerCase().includes(searchUser.toLowerCase())
+        :
+        null
+    ))
+  }, [users, mediaFilter, searchUser, searchByUser])
+  
+  const showAllUsers = (e) => {
+    setShowUsers(users)
+  }
 
   // to collaborate, logged in user clicks a button on user's profile that takes you to a 
   // create collaboration page.  on that page, the user can choose the media, and from a 
@@ -26,8 +37,14 @@ export default function Users(props) {
   return (
     <UserList>
       <h1>users</h1>
-      <form>
-        <select onChange={(e) => setMediaFilter([...mediaFilter, e.target.value]) }>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={searchUser}
+          onChange={(e) => setSearchUser(e.target.value)}
+        />
+        <select onChange={(e) => setMediaFilter([...mediaFilter, e.target.value])}>
+        <option disabled value='default' >find by media</option>
           {media.map(medium => 
             <option value={medium.name}>{ medium.name}</option>
             )}
@@ -38,6 +55,12 @@ export default function Users(props) {
           <Link  className="user-link" key={user.id} to={`/users/${user.id}`}>
             <img  className="user-thumbnail-img" src={ user.img_url ? user.img_url : `https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60`}/>
             <p className="thumbnail-username">{user.username}</p>
+            <p>matching media:</p>
+            {user.media.filter(medium => 
+              mediaFilter.includes(medium.name)
+            ).map(medium => 
+              <p>{ medium.name}</p>
+              )}
           </Link>
         </div>
       )}
